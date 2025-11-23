32af30b6a332e222039b2af4a3bac3c6630eb00b
Rick Correia <121581239+BlastHappy82@users.noreply.github.com>
2025-11-23 01:31:12 +0000

    Fixed Slice75 HE Layout

--- src/utils/sharedLayout.ts
+++ src/utils/sharedLayout.ts
@@ -17,10 +17,19 @@ export const sharedLayoutMap: Record<string, LayoutConfig> = {
       [27.5].concat(Array(12).fill(19.05), 27.5, 19.05),
       [32.3].concat(Array(11).fill(19.05), 42.8625),
       [42.8625].concat(Array(10).fill(19.05), 32.3, 19.05),
-      Array(3).fill(23.8125).concat(119.38, 23.8125, 23.8125, Array(3).fill(19.05)),
+      Array(3)
+        .fill(23.8125)
+        .concat(119.38, 23.8125, 23.8125, Array(3).fill(19.05)),
     ],
-    gapsAfterCol: [{ 0: 4.25, 4: 4.25, 8: 4.25, 12: 4.25 }, {}, {}, {}, {}, { 5: 13.1 }],
-    rowSpacing: Array(6).fill(20.05),
-    hasAxisList: true
-  }
+    gapsAfterCol: [
+      { 0: 4.25, 4: 4.25, 8: 4.25, 12: 4.25 },
+      {},
+      {},
+      {},
+      {},
+      { 5: 13.4 },
+    ],
+    rowSpacing: [24.3].concat(Array(5).fill(20.05)),
+    hasAxisList: true,
+  },
 };
